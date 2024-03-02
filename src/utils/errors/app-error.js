e43bf01198ea7237658caa8eddfa7c5a0020fe5d
gaurav <gaurav30012000jha@gmail.com>
2024-03-02 21:24:39 +0530

    Added migration to add new columns to booking table

--- src/utils/errors/app-error.js
+++ src/utils/errors/app-error.js
@@ -12,5 +12,5 @@ class AppError extends Error {
         this.statusCode=statusCode;
     }
 }
-
+ 
 module.exports= AppError;
---
+++ src/utils/index.js
@@ -0,0 +1,6 @@
+module.exports =
+{
+    ValidationError:require('./errors/validation-error'),
+    AppError:require('./errors/app-error'),
+    ServiceError:require('./errors/service-error')
+}
